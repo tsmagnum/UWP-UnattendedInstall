2a0d84916ff4c6c11c380325f0d9e79d79910aaf
Federico Lillacci <65031947+tsmagnum@users.noreply.github.com>
2024-08-08 12:22:07 +0200

    Create Create-TaskInstallAppX.ps1

---
+++ Create-TaskInstallAppX.ps1
@@ -0,0 +1,22 @@
+#Script Create-TaskInstallAppX.ps1
+
+#Preparazione della cartella per lo script e copia del file
+If (!(Test-Path -Path C:\Scripts))
+{
+    New-Item -ItemType directory -Path C:\Scripts
+}
+
+New-PSDrive -Name S -PSProvider FileSystem -Root "\\server\folder\Satispay" -Credential $using:creds
+
+Copy-Item -Path "S:\Install-SatispayAppX.ps1" -Destination C:\Scripts -Force
+
+#Selezione dell'utente loggato in console per l'esecuzione del task
+$user = (Get-CimInstance -ClassName Win32_ComputerSystem | Select-Object username).username
+
+$computer = $env:COMPUTERNAME
+
+#Impostazione dell'ora di esecuzione del task a +1 minuto dall'esecuzione dello script
+$time = (Get-Date).AddMinutes(1).ToString('HH:mm')
+
+#Creazione del task schedulato
+schtasks /create /s $computer /tn "Install-Satispay" /sc once /tr "powershell.exe -ExecutionPolicy bypass -WindowStyle hidden -file C:\Scripts\Install-SatispayAppX.ps1" /st $time /ru $user
